4d09d9b9a7799450fa273b1d9740654c7fd75771
Great <alabigreat@gmail.com>
2022-09-13 17:40:07 -0700

    nested

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,27 +1,31 @@
 #include "main.h"
 
 /**
- *	 main - Program entry point
- *
- *	 Description: This is a program that will print the
- *	 the message "_putchar" to the screen followed by a
- *	 newline. It uses a locally defined function
- *	 (_putchar) to accomplish this.
- *
- *       Return: Always 0 (If Successful)
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 
 {
-		_putchar('_');
-		_putchar('p');
-		_putchar('u');
-		_putchar('t');
-		_putchar('c');
-		_putchar('h');
-		_putchar('a');
-		_putchar('r');
-		_putchar('\n');
-		return (0);
+
+	_putchar('_');
+
+	_putchar('p');
+
+	_putchar('u');
+
+	_putchar('t');
+
+	_putchar('c');
+
+	_putchar('h');
+
+	_putchar('a');
+
+	_putchar('r');
+
+	_putchar('\n');
+
+	return (0);
 }
