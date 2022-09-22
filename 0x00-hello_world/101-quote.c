5848256c58077ce8c5813e8366279d626b628224
Great <alabigreat@gmail.com>
2022-09-22 01:26:47 -0700

    hello

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - entry point of the programme
- *
- * Description:the program should
- * print 'and that piece of art
- * is useful'- dora korpar, 2015-10-19
- *
- * Return: program end with an output 1
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-putchar("and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19"\n");
-return (1);
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
