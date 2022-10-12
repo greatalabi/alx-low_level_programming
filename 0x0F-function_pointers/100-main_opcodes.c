3068b8571a48749901d7a936fe725a151e14006c
Great <alabigreat@gmail.com>
2022-10-12 15:34:47 -0700

    Function pointers

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main entry point into the program
+ * @argc: number of command line arguments
+ * @argv: vector containing command line arguments
+ *
+ * Return: Always 0 (Successful)
+ */
+
+int main(int argc, char *argv[])
+{
+	int bytes;
+	char *arr;
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	arr = (char *)main;
+
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
+	return (0);
+}
